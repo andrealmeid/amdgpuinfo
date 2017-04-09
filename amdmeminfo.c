92ba22ee685215914d96c72abb58088de1f05f5a
Sling00 <sling00@gmail.com>
2017-04-09 03:52:19 -0500

    add rx vega pci id

--- amdmeminfo.c
+++ amdmeminfo.c
@@ -128,6 +128,8 @@ typedef struct {
 } gputype_t;
 
 static gputype_t gputypes[] = {
+    /* Vega */
+    { 0x1002, 0x687f, 0, "Radeon RX Vega"},
     /* Fury/Nano */
     { 0x1002, 0x7300, 0, "Radeon R9 Fury/Nano/X"},
     { 0x1002, 0x7300, 0xc8, "Radeon R9 Fury/Nano/X"},
