4e7569be32812473f6e211dc065591cedcc8a60b
Sling00 <sling00@gmail.com>
2017-04-18 23:41:24 -0500

    add rx 570/580

--- amdmeminfo.c
+++ amdmeminfo.c
@@ -136,6 +136,9 @@ static gputype_t gputypes[] = {
     { 0x1002, 0x7300, 0xc9, "Radeon R9 Fury/Nano/X"},
     { 0x1002, 0x7300, 0xca, "Radeon R9 Fury/Nano/X"},
     { 0x1002, 0x7300, 0xcb, "Radeon R9 Fury/Nano/X"},
+    /* RX 5xx */
+    { 0x1002, 0x67df, 0xe7, "Radeon RX 580"},
+    { 0x1002, 0x67df, 0xef, "Radeon RX 570"},
     /* RX 4xx */
     { 0x1002, 0x67df, 0, "Radeon RX 470/480"},
     { 0x1002, 0x67df, 0xc7, "Radeon RX 480"},
